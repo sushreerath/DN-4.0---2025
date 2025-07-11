9ebed15a8c95064a2d4969c6f39921de8b57e885
sushreerath <rathsuman503@gmail.com>
2025-07-12 00:02:34 +0530

    Added week4 folder with Spring projects

---
+++ WEEK4/week4/spring-learn/src/test/java/com/cognizant/spring_learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learn;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringLearnApplicationTests {
+
+	@Test
+    public void contextLoads() {
+	}
+
+}
